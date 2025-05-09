bf7a6fbc342d549b6c93bc6dad7d388c3bc55142
shanelze <85552763+shanelze@users.noreply.github.com>
2025-05-09 11:29:16 +0800

    Delete coursework - scam detection directory

--- coursework - scam detection/code/app.py
+++
@@ -1,7 +0,0 @@
-from website import create_app
-
-app = create_app ()
-
-if __name__ == 'main':
-    app.secret_key = 'BC3415'
-    app.run (debug=True)
--- coursework - scam detection/code/website/pages/rewards.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-
-rewards_page = Blueprint ('rewards_page', __name__, template_folder='/templates')
-
-@rewards_page.route ('/rewards', methods = ['GET'])
-@login_required
-def rewards ():
-    return render_template ("rewards.html", user=current_user)
